5053581343402109140047c2478790bc1c7306fc
malaikaMI <malaikamic@gmail.com>
2018-06-26 22:26:36 +0500

    RUBY ON RAILS PROJECT,BLOGGER

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles do 
+  	resources :comments
+  end
+
+  root to: 'articles#index'
+  resources :tags
+  resources :authors
+  resources :author_sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+end
